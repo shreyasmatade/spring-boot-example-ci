3cb6ce58fefb7d5088bf8fc25be652f9a50c043c
shreyasmatade <shreyas.s.matade@gmail.com>
2023-06-18 17:53:02 -0500

    Publishing Version from App

--- src/main/java/com/example/ci/exampleci/controllers/GreetingsController.java
+++ src/main/java/com/example/ci/exampleci/controllers/GreetingsController.java
@@ -1,17 +1,25 @@
 package com.example.ci.exampleci.controllers;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingsController {
 
+    @Value("${app.version}")
+    private String appVersion;
+
 	@GetMapping("/")
 	public Map<String, Object> greeting() {
-		return Collections.singletonMap("message", "Hello, World");
+        Map<String, Object> res = new HashMap<>();
+        res.put("message", "Hello, World");
+        res.put("version", appVersion);
+		return res;
 	}
 
 }
