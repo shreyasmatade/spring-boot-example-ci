3e59d49f9b1ffd7a446b16dd5e487766f2da5550
shreyasmatade <shreyas.s.matade@gmail.com>
2023-06-18 19:50:12 -0500

    Fixed auto Versioning

--- src/main/java/com/example/ci/exampleci/controllers/GreetingsController.java
+++ src/main/java/com/example/ci/exampleci/controllers/GreetingsController.java
@@ -1,24 +1,27 @@
 package com.example.ci.exampleci.controllers;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Component
 @RestController
 public class GreetingsController {
 
-    @Value("${app.version}")
-    private String appVersion;
+    @Value("${app.version:local-build}")
+    String appVersion;
+
 
 	@GetMapping("/")
 	public Map<String, Object> greeting() {
         Map<String, Object> res = new HashMap<>();
         res.put("message", "Hello, World");
-        res.put("version", appVersion);
+        res.put("version",appVersion );
 		return res;
 	}
 
